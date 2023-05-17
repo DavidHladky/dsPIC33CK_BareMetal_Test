eb8834b6df0e996f9b10ef1b9692bebc949ca917
xhladk08 <hladkyd@gmail.com>
2023-05-17 22:47:44 +0200

    New project established!

---
+++ DSPIC33CK_BareMetal_test.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=DSPIC33CK_BareMetal_test.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/DSPIC33CK_BareMetal_test.X.production.hex
